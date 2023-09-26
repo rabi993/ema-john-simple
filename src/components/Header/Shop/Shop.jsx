a271537d8c7b645b482debb92d23c11f7ba31e41
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-09-26 19:37:24 +0600

    handle add to cart & fave icon

--- src/components/Header/Shop/Shop.jsx
+++ src/components/Header/Shop/Shop.jsx
@@ -5,6 +5,7 @@ import Product from '../../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
 
     useEffect( ()=>{
         fetch('products.json')
@@ -13,6 +14,13 @@ const Shop = () => {
 
 
     },[])
+
+    const handleAddToCart =(product)=> {
+        const newCart =[...cart, product];
+        setCart(newCart);
+
+    }
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
@@ -21,6 +29,7 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product ={product}
+                        handleAddToCart = {handleAddToCart}
                     ></Product>)
                 }
 
@@ -28,6 +37,7 @@ const Shop = () => {
             <div className="cart-container">
 
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,22 +1,25 @@
 /* eslint-disable no-unused-vars */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css';
 
-const Product = (props) => {
-    const {img, name, seller, ratings, price} = props.product;
+function Product(props) {
+    const { img, name, seller, ratings, price } = props.product;
 
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
-            <h6 className='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>Menufacturer: {seller}</p>
-            <p>Ratings: {ratings} stars</p>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Menufacturer: {seller}</p>
+                <p>Ratings: {ratings} stars</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart <FontAwesomeIcon icon={faShoppingCart}/> </button>
         </div>
     );
-};
+}
 
 export default Product;
