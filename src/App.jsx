65e4468a89240dcab9f705b03e88756e15d64454
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-09-26 17:06:30 +0600

    load products by fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Header/Shop/Shop'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header></Header> 
+      <Shop></Shop>
     </>
   )
 }
